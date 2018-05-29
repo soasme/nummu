82d3e363f67b86f84fa4bf4c6b9108b42b6eb3c2
soasme <soasme@gmail.com>
2018-05-29 22:31:22 +1200

    add line example.

---
+++ examples/line.py
@@ -0,0 +1,33 @@
+import numpy as np
+from nummu import draw
+
+class Line:
+
+    def init(self):
+        self.y1 = 0
+        self.y2 = 100
+
+    def update(self, delta):
+        self.y1 += delta
+        self.y2 -= delta
+        if self.y1 >= 100 or self.y2 <= 0:
+            raise StopIteration
+
+    def draw(self, pallete):
+        x1, y1, x2, y2 = 0, self.y1, 100, self.y2
+        color = (255, 255, 255)
+        draw.line(pallete, x1, y1, x2, y2, color=color)
+
+
+def main():
+    from nummu import Nummu
+    nm = Nummu(100, 100)
+    nm.extend(Line())
+    nm.export('line.gif', delay=4)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
