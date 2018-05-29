744aa562c274d75f8395faa77102591b650dcffa
Soasme <soasme@users.noreply.github.com>
2018-05-29 14:54:44 +1200

    Update rect.py

--- examples/rect.py
+++ examples/rect.py
@@ -5,15 +5,18 @@ class Rect:
 
     def init(self):
         self.size = 0
+        self.pos = 50
 
     def update(self, delta):
         self.size += delta
+        self.pos = (100 - self.size) / 2
         if self.size >= 100:
             raise StopIteration
 
     def draw(self, pallete):
-        pos = (100 - self.size) / 2
-        draw.rect(pallete, pos, pos, self.size, self.size, color=(255, 255, 255))
+        x, y, w, h = self.pos, self.pos, self.size, self.size
+        color = (255, 255, 255)
+        draw.rect(pallete, x, y, w, h, color=color)
 
 
 def main():
